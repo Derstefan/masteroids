0affea207ad601e26ca66d8c57eaa4ec3fc4f6ff
StefanW <stefanwetzig@yahoo.de>
2024-01-16 19:58:11 +0100

    add camerafollow and map borders

---
+++ Assets/Scenes/Game/Scripts/Map/EuclideanTorusScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EuclideanTorusScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Teleport the game object
+        if (transform.position.x > MapConfig.XMax)
+        {
+
+            transform.position = new Vector3(MapConfig.XMin, transform.position.y, 0);
+
+        }
+        else if (transform.position.x < MapConfig.XMin)
+        {
+            transform.position = new Vector3(MapConfig.XMax, transform.position.y, 0);
+        }
+
+        else if (transform.position.y > MapConfig.YMax)
+        {
+            transform.position = new Vector3(transform.position.x, MapConfig.YMin, 0);
+        }
+
+        else if (transform.position.y < MapConfig.YMin)
+        {
+            transform.position = new Vector3(transform.position.x, MapConfig.XMax, 0);
+        }
+    }
+}
